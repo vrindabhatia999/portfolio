ab6b751cc1ebce2a615c1112d4706a4f5569a8d3
Vrinda Bhatia <vrindabhatia@Vrindas-Laptop.local>
2024-09-08 02:06:31 +0530

    feat: Portfolio app

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { MuiThemeProvider, useMediaQuery, CssBaseline } from '@material-ui/core';
-import { darkTheme, lightTheme } from '../src/theme';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import {
+  MuiThemeProvider,
+  useMediaQuery,
+  CssBaseline,
+} from "@material-ui/core";
+import { darkTheme, lightTheme } from "../src/theme";
 
 export default function MyApp({ Component, pageProps }) {
-
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
-  const [theme, setTheme] = useState(
-    prefersDarkMode ? darkTheme : lightTheme
-  )
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const [theme, setTheme] = useState(prefersDarkMode ? darkTheme : lightTheme);
 
   useEffect(() => {
-    setTheme(prefersDarkMode ? darkTheme : lightTheme)
-  }, [prefersDarkMode])
+    setTheme(prefersDarkMode ? darkTheme : lightTheme);
+  }, [prefersDarkMode]);
 
   useEffect(() => {
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
@@ -26,12 +27,15 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>Kaustubh Odak</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <title>Vrinda Bhatia</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
       </Head>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
-        <Component {...pageProps} setTheme={setTheme}/>
+        <Component {...pageProps} setTheme={setTheme} />
       </MuiThemeProvider>
     </React.Fragment>
   );
--- src/Projects.js
+++ src/Projects.js
@@ -1,109 +1,122 @@
-import { Card, CardActionArea, CardActions, CardContent, CardHeader, Chip, Fade, Grid, Hidden, makeStyles, Typography } from "@material-ui/core";
-import { RepoForkedIcon, RepoIcon, StarIcon } from '@primer/octicons-react';
-import Image from 'next/image'
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Chip,
+  Fade,
+  Grid,
+  Hidden,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import { RepoForkedIcon, RepoIcon, StarIcon } from "@primer/octicons-react";
+import Image from "next/image";
 import { useRef } from "react";
 import useAnimate from "./useAnimate";
+import Link from "next/link";
 
-const useStyles = makeStyles(theme => ({
-    cont: {
-        minHeight: `calc(100vh - ${theme.spacing(4)}px)`,
+const useStyles = makeStyles((theme) => ({
+  cont: {
+    minHeight: `calc(100vh - ${theme.spacing(4)}px)`,
+  },
+  card: {
+    height: "100%",
+  },
+  cardActionArea: {
+    height: "100%",
+    // display: 'grid'
+  },
+}));
+
+const projectData = {
+  url: "https://github.com/vrindabhatia999",
+  repositories: [
+    { link: "", name: "Portfolio" },
+    {
+      link: "https://travel-itenary-vrindabhatia999.netlify.app/",
+      name: "Travel-Itenary",
     },
-    card: {
-        height: '100%'
+    {
+      link: "https://nft.rakuten.co.jp/moments/JL_1649331419_7160633c3/",
+      name: "Video Max",
     },
-    cardActionArea: {
-        height: '100%',
-        // display: 'grid'
-    }
-}))
+    {
+      link: "https://check-your-weather-vrindabhatia999.netlify.app/",
+      name: "Weather App",
+    },
+  ],
+};
 
 export default function Projects({ data }) {
+  const classes = useStyles();
+  console.log("data", data);
+  const animRef = useRef(null);
+  const animate = useAnimate(animRef);
 
-    const classes = useStyles()
-
-    const animRef = useRef(null)
-    const animate = useAnimate(animRef)
-
-    return (
-        <Grid direction="row-reverse" container justify="center" alignItems="center" spacing={10} className={classes.cont}>
-            <Grid item xs={12} lg={6}>
-                <Typography variant="h2" gutterBottom align="center" innerRef={animRef}>
-                    Projects
-                </Typography>
-                <Hidden mdDown>
-                    <Fade in={animate} style={{ transitionDelay: '250ms' }}>
-                        <div>
-                            <Image
-                                alt="Projects"
-                                src="/projects.svg"
-                                width="1144"
-                                height="617.32"
-                            />
-                        </div>
-                    </Fade>
-                </Hidden>
-            </Grid>
-            <Grid container item xs={12} lg={6} direction="row" spacing={1}>
-                {
-                    !!data && data.map((v, i) =>
-                        <Grid item sm={6} xs={12} key={i}>
-                            <Fade in={animate} style={{ transitionDelay: `${200 * i}ms` }}>
-                                <Card key={i} className={classes.card}>
-                                    <CardActionArea
-                                        className={classes.cardActionArea}
-                                        href={v.value.html_url}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        <CardHeader
-                                            title={<><RepoIcon verticalAlign='middle' /> {v.value.name}</>}
-                                            subheader={
-                                                <>
-                                                    {
-                                                        !!v.value.stargazers_count &&
-                                                        <>
-                                                            <StarIcon verticalAlign='middle' />
-                                                            {v.value.stargazers_count}
-                                                        </>
-                                                    }
-                                                    {
-                                                        !!v.value.forks &&
-                                                        <>
-                                                            <RepoForkedIcon verticalAlign='middle' />
-                                                            {v.value.forks}
-                                                        </>
-                                                    }
-                                                </>
-                                            }
-                                        />
-                                        <CardContent>
-                                            <Typography variant="body2" color="textSecondary" component="p">
-                                                {v.value.description}
-                                            </Typography>
-                                        </CardContent>
-                                        <CardActions>
-                                            <Grid container direction="row" spacing={1}>
-                                                {
-                                                    !!v.value.languages &&
-                                                    v.value.languages.map((lang, i) =>
-                                                        <Grid item key={i}>
-                                                            <Chip
-                                                                key={i}
-                                                                label={lang}
-                                                                size="small"
-                                                            />
-                                                        </Grid>
-                                                    )
-                                                }
-                                            </Grid>
-                                        </CardActions>
-                                    </CardActionArea>
-                                </Card>
-                            </Fade>
-                        </Grid>
-                    )
-                }
-            </Grid>
-        </Grid>
-    )
+  return (
+    <Grid
+      direction="row-reverse"
+      container
+      justify="center"
+      alignItems="center"
+      spacing={10}
+      className={classes.cont}
+    >
+      <Grid item xs={12} lg={6}>
+        <Typography variant="h2" gutterBottom align="center" innerRef={animRef}>
+          Projects
+        </Typography>
+        <Hidden mdDown>
+          <Fade in={animate} style={{ transitionDelay: "250ms" }}>
+            <div>
+              <Image
+                alt="Projects"
+                src="/projects.svg"
+                width="1144"
+                height="617.32"
+              />
+            </div>
+          </Fade>
+        </Hidden>
+      </Grid>
+      <Grid
+        container
+        item
+        xs={12}
+        lg={6}
+        direction="row"
+        spacing={1}
+        style={{ marginTop: "100px" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "40px",
+            width: "100%",
+          }}
+        >
+          {!!projectData &&
+            projectData.repositories.map((v, i) => (
+              <div
+                style={{
+                  width: "100%",
+                  background: "#23312E",
+                  padding: "12px 12px",
+                  display: "flex",
+                  gap: "10px",
+                }}
+              >
+                <label>{v.name}</label>
+                <a style={{ color: "white" }} target="_blank" href={v.link}>
+                  Click here
+                </a>
+              </div>
+            ))}
+        </div>
+      </Grid>
+    </Grid>
+  );
 }
